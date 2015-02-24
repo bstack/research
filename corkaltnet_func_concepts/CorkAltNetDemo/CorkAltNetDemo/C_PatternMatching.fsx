921ff990ed0cb9dfa7b22b833d4436279229f007
bstack <bs.stack@gmail.com>
2015-02-24 11:59:43 +0000

    Updated with latest version of code

--- corkaltnet_func_concepts/CorkAltNetDemo/CorkAltNetDemo/C_PatternMatching.fsx
+++ corkaltnet_func_concepts/CorkAltNetDemo/CorkAltNetDemo/C_PatternMatching.fsx
@@ -1,1 +1,41 @@
 ﻿
+
+// demonstrating pattern matching
+let filter123 x =
+    match x with
+    | 1 | 2 | 3 -> printfn "Found 1, 2, or 3!"
+    | a -> printfn "%d" a
+
+let filterWithWhen x =
+    match x with
+    | 1 | 2 | 3 -> printfn "Found 1, 2, or 3!"
+    | withinValidNumberRange when withinValidNumberRange > 0 -> printfn "Found a valid number"
+    | _ -> printfn "The test value is out of range."
+
+let filterWithError x =
+    match x with
+    | 1 | 2 | 3 -> printfn "Found 1, 2, or 3!"
+    | withinValidNumberRange when withinValidNumberRange > 0 -> printfn "Found a valid number"
+    | _ -> failwith "Unknown report type" "The test value is out of range."
+
+let x = Some 99
+let result = match x with 
+| Some i -> i * 2
+| None -> 0
+
+
+
+
+
+
+// demonstrating recursion and pattern matching
+let list1 = [ 1; 5; 100; 450; 788 ]
+
+// Pattern matching by using the cons pattern and a list 
+// pattern that tests for an empty list. 
+let rec printList listx =
+    match listx with
+    | head :: tail -> printf "%d " head; printList tail
+    | [] -> printfn ""
+
+printList list1
--- corkaltnet_func_concepts/CorkAltNetDemo/CorkAltNetDemo/H_EulerProblem2.fsx
+++ corkaltnet_func_concepts/CorkAltNetDemo/CorkAltNetDemo/H_EulerProblem2.fsx
@@ -1,1 +1,22 @@
 ﻿
+// Euler problem 5: https://projecteuler.net/problem=5
+//
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+let getRemainder subject divisor =
+    subject % divisor
+
+let getRemainderFor1To20 sub:int = 
+    let countSequence = seq {1 .. 20}
+    let remainderMap = Seq.map (fun(element) -> getRemainder sub element) countSequence
+    let remainderValue = remainderMap |> Seq.sum
+    remainderValue
+
+let rec getAnswer n =
+    let remainder = getRemainderFor1To20 n
+    printfn "Processed number: %d which has remainder: %d" n remainder 
+    if remainder = 0 then n
+    else getAnswer (n+1)
+
+getAnswer 1
